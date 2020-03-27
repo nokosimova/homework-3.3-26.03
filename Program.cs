b9b4937511287272f90535a5949688fda826a2dd
unknown <nkosimova06@gmail.com>
2020-03-27 20:57:25 +0500

    hw3problem3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem 3:
+            decimal a, b, c;
+            String[] nums = Console.ReadLine().Split();
+            a = decimal.Parse(nums[0]);
+            b = decimal.Parse(nums[1]);
+            c = decimal.Parse(nums[2]);
+ 
+        }
+    }
+}
