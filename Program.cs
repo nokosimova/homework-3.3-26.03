24bf92bb84a056ba3e751b86d8aa9588c1f4de7e
unknown <nkosimova06@gmail.com>
2020-03-27 21:07:39 +0500

    hw3problem3 first

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ namespace Problem3
             a = decimal.Parse(nums[0]);
             b = decimal.Parse(nums[1]);
             c = decimal.Parse(nums[2]);
+            decimal min_el = Math.Min(Math.Min(a, b), c);
+            decimal max_el = Math.Max(Math.Max(a,b), c);
+            decimal mid_el = a + b + c - max_el - min_el;
+            a = min_el;
+            b = mid_el;
+            c = max_el;
+            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
  
         }
     }
