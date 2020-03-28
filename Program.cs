7c9375901b1b8743e7455df110c42cd0c2e459fe
unknown <nkosimova06@gmail.com>
2020-03-28 17:44:15 +0500

    correct

--- Program.cs
+++ Program.cs
@@ -12,12 +12,14 @@ namespace Problem3
             a = decimal.Parse(nums[0]);
             b = decimal.Parse(nums[1]);
             c = decimal.Parse(nums[2]);
+            
+           // Alternative solution without "If":
             decimal min_el = Math.Min(Math.Min(a, b), c);
-            decimal max_el = Math.Max(Math.Max(a,b), c);
+            decimal max_el = Math.Max(Math.Max(a, b), c);
             decimal mid_el = a + b + c - max_el - min_el;
-            a = min_el;
+            a = max_el;
             b = mid_el;
-            c = max_el;
+            c = min_el;
             Console.WriteLine($"a = {a}, b = {b}, c = {c}");
  
         }
